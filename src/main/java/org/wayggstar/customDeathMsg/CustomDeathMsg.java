5141e8f0ac5aff0fbb62154df6ff8a97223752a1
wayggstar <wayggstar@gmail.com>
2024-12-23 17:32:04 +0400

    Initial commit

---
+++ src/main/java/org/wayggstar/customDeathMsg/CustomDeathMsg.java
@@ -0,0 +1,16 @@
+package org.wayggstar.customDeathMsg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomDeathMsg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
